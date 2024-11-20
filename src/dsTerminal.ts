514879cc0755dd5e07fc9d2b0533ae8b2b2d0b5e
Michael Gentili <gentili@mcpnet.ca>
2024-11-20 18:08:29 +0000

    Factor Terminal out of the Kernel

--- src/dsTerminal.ts
+++ src/dsTerminal.ts
@@ -1,28 +1,34 @@
-import { FitAddon } from "@xterm/addon-fit";
-import { WebglAddon } from "@xterm/addon-webgl";
-import { Terminal } from "@xterm/xterm";
-import '@xterm/xterm/css/xterm.css';
+import { Terminal } from '@xterm/xterm';
+import { WebglAddon } from '@xterm/addon-webgl';
+import { FitAddon } from '@xterm/addon-fit';
+
+import { DSKernel } from './dsKernel';
 
 function isMobileDevice(): boolean {
     const userAgent = navigator.userAgent;
-  
+
     // Check for Android or iOS
     return /android/i.test(userAgent) || /iPad|iPhone|iPod/.test(userAgent);
 }
 
-export class DepSysOS {
+export class DSTerminal {
     private terminal: Terminal;
     private webglAddon: WebglAddon;
     private fitAddon: FitAddon;
 
-    private handleResize = () => {
-        this._resizeTerminal();
-        const t = this.terminal;
-        t.writeln(`Resize: ${t.cols} x ${t.rows}`);
-    };
+    get cols(): number  {
+        return this.terminal.cols;
+    }
 
-    constructor(terminalContainer: HTMLDivElement) {
+    get rows(): number  {
+        return this.terminal.rows;
+    }
+
+
+    // Private constructor to prevent direct instantiation
+    constructor(private kernel : DSKernel, terminalContainer: HTMLDivElement) {
         // Initialize the xterm Terminal
+
         const t = this.terminal = new Terminal(
             {
                 cols: 20,              // Set the number of columns (width)
@@ -36,10 +42,11 @@ export class DepSysOS {
         );
 
         // Open the terminal in the specified container
-        this.webglAddon = new WebglAddon();
         if (!isMobileDevice()) {
+            this.webglAddon = new WebglAddon();
             t.loadAddon(this.webglAddon);
         }
+
         this.fitAddon = new FitAddon();
         t.loadAddon(this.fitAddon);
 
@@ -50,30 +57,31 @@ export class DepSysOS {
             foreground: '#00ff00', // Green font color
             background: '#000000', // Black background
         };
-        console.log("DepSys terminal firmware initialized");
 
-        console.log("Booting DepSysOS...");
-
-        t.writeln("DepSysOS Booting...");
-
-        this._resizeTerminal();
-
-        t.writeln('');
-        t.writeln(`Device : ${navigator.userAgent}`);
-        t.writeln(`Grid   : ${t.cols} X ${t.rows}`);
-        /*
-        for (let i = 0; i < t.cols; i++) {
-            t.write(String(i % 10));
-        }
-        t.writeln("");
-        for (let i = 0; i < t.cols; i++) {
-            t.write(String(i / 10).charAt(0));
-        }
-        */
+        // Call resize directly (no propagation because we're booting)
+        this._resize();
         window.addEventListener('resize', this.handleResize);
+        
+        // TODO: Add listeners for keystrokes, clicks, and touches
     }
 
-    private _resizeTerminal(): void {
+    async baudText(msg: string, delay: number = 5): Promise<void> {
+        for (const char of msg) {
+            this.terminal.write(char);
+            await new Promise(resolve => setTimeout(resolve, delay));
+        }
+    }
+
+    directText(msg: string) {
+        this.terminal.write(msg);
+    }
+
+    handleResize = () => {
+        this._resize();
+        this.kernel.handleResize();
+    }
+
+    private _resize(): void {
         const t = this.terminal;
         const portrait = window.matchMedia("(orientation: portrait)").matches;
         const targetCols = portrait ? 40 : 80;
@@ -82,15 +90,26 @@ export class DepSysOS {
         // Find out what the new dimensions should be
         let newdim = this.fitAddon.proposeDimensions();
         // If they're too far from 80 cols
-        while (newdim.cols > targetCols+1) {
+        while (newdim.cols > targetCols + 1) {
             t.options.fontSize++;
             newdim = this.fitAddon.proposeDimensions();
         }
-        while (newdim.cols < targetCols+1) {
+        while (newdim.cols < targetCols + 1) {
             t.options.fontSize--;
             newdim = this.fitAddon.proposeDimensions();
         }
 
         t.resize(newdim.cols - 1, newdim.rows);
     }
+
+            /*
+        for (let i = 0; i < t.cols; i++) {
+            t.write(String(i % 10));
+        }
+        t.writeln("");
+        for (let i = 0; i < t.cols; i++) {
+            t.write(String(i / 10).charAt(0));
+        }
+        */
+
 }
