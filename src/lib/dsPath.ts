35e84142667cfa5dc39fb2a54734b31c1902ceb8
Nicholas Waslander <74473358+Nicholas-py@users.noreply.github.com>
2025-09-22 14:50:35 -0400

    53 add relative path support for dsmdbrowser (#69)
    * Functionality
    
    * Set index.dsmd to use relative links
    
    * Add image support via complete rework
    
    * Rearrange code
    
    * Refactor + bugfix
    
    * Move getabsolute path to + add tests for dspath

---
+++ src/lib/dsPath.ts
@@ -0,0 +1,30 @@
+import { DSIDirectory } from "../dsFileSystem";
+
+export function getDirPath(filepath: string): string {
+    if (filepath.lastIndexOf('/') != -1) {
+        return filepath.slice(0, filepath.lastIndexOf('/'));
+    }
+    else {
+        return '.'
+    }
+}
+
+export function getFileName(filepath: string): string {
+        if (filepath.lastIndexOf('/') != -1) {
+            return filepath.slice(filepath.lastIndexOf('/') + 1);
+        }
+        else {
+            return filepath
+        }
+    }
+
+export function getAbsolutePath(localdir:DSIDirectory, path: string) {
+        const dirpath = getDirPath(path);
+        const globaldirpath = localdir.getdir(dirpath).path;
+        const filename = getFileName(path);
+        
+        if (globaldirpath == '/') {
+            return '/' + filename;
+        }
+        return globaldirpath + '/' + filename;
+    }
