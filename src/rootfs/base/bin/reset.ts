685025d51fa3f2bda2b4795e8f9baf15d56fc684
Nicholas Waslander <74473358+Nicholas-py@users.noreply.github.com>
2025-12-29 18:34:17 -0500

    87 implement layered rootfs (#94)
    * Reorganize filesystem
    
    * Get genrootfs to work
    
    * Remove commented code
    
    * Remove bindir
    
    * Fix up a couple links
    
    ---------
    
    Co-authored-by: Michael Gentili <gentili@mcpnet.ca>

---
+++ src/rootfs/base/bin/reset.ts
@@ -0,0 +1,17 @@
+import { DSKernel } from "../../../dsKernel";
+import { DSProcess } from "../../../dsProcess";
+import { DSOptionParser } from "../../../lib/dsOptionParser";
+
+export class PRReset extends DSProcess {
+
+    protected async main(): Promise<void> {
+        const optparser = new DSOptionParser(
+            this.procname,
+            true,
+            "   Reset the terminal"
+        );
+        let nextarg = optparser.parseWithUsageAndHelp(this.argv);
+
+        DSKernel.terminal.reset();
+    }
+}
