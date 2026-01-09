fe5278682ddff545aa5f4a4004656bfc28914c1a
Nicholas Waslander <74473358+Nicholas-py@users.noreply.github.com>
2026-01-09 12:11:07 -0800

    67 add filesystem viewer (#85)
    * Add file
    
    * Add interface
    
    * Add images:
    
    https://www.flaticon.com/free-icon/folder_1383970
    https://www.flaticon.com/free-icons/picture
    https://www.flaticon.com/free-icon/file_2521610
    
    * Imgview gif support (bonus feature)
    
    * Add icons
    
    * Add history safety, arrow key support, and scrolling
    
    * Bugfixes
    
    * Man page + tweaks
    
    * Bugfix - files displaying twice on load
    
    * Add to man page + fix bug
    
    * Add to demomarkdown
    
    * Fix man page line spacing
    
    * Add file
    
    * Add interface
    
    * Add images:
    
    https://www.flaticon.com/free-icon/folder_1383970
    https://www.flaticon.com/free-icons/picture
    https://www.flaticon.com/free-icon/file_2521610
    
    * Imgview gif support (bonus feature)
    
    * Add icons
    
    * Add history safety, arrow key support, and scrolling
    
    * Bugfixes
    
    * Man page + tweaks
    
    * Bugfix - files displaying twice on load
    
    * Add to man page + fix bug
    
    * Add to demomarkdown
    
    * Fix man page line spacing
    
    * Fix fsviewer folder somewhere weird

--- src/dsRootFS.ts
+++ src/dsRootFS.ts
@@ -22,6 +22,7 @@ import { DSShell } from "./rootfs/base/bin/dssh";
 import { PREcho } from "./rootfs/base/bin/echo";
 import { PREnv } from "./rootfs/base/bin/env";
 import { PRFile } from "./rootfs/base/bin/file";
+import { PRFSViewer } from "./rootfs/base/bin/fsviewer";
 import { PRImgview } from "./rootfs/base/bin/imgview";
 import { PRInit } from "./rootfs/base/bin/init";
 import { PRLess } from "./rootfs/base/bin/less";
@@ -75,6 +76,10 @@ import rootfs_base_data_app_pixel_assault_shieldpiece_png from "./rootfs/base/da
 import rootfs_base_data_app_pixel_assault_shieldpiece10_png from "./rootfs/base/data/app/pixel_assault/shieldpiece10.png";
 import rootfs_base_data_app_pixel_assault_shieldpiece3_png from "./rootfs/base/data/app/pixel_assault/shieldpiece3.png";
 import rootfs_base_data_app_pixel_assault_shieldpiece6_png from "./rootfs/base/data/app/pixel_assault/shieldpiece6.png";
+import rootfs_site_data_app_fsviewer_foldericon_png from "./rootfs/site/data/app/fsviewer/foldericon.png";
+import rootfs_site_data_app_fsviewer_fsvieweropen_dsmd from "./rootfs/site/data/app/fsviewer/fsvieweropen.dsmd";
+import rootfs_site_data_app_fsviewer_imageicon_png from "./rootfs/site/data/app/fsviewer/imageicon.png";
+import rootfs_site_data_app_fsviewer_texticon_png from "./rootfs/site/data/app/fsviewer/texticon.png";
 import rootfs_base_data_demo_animation_32x32_A_png from "./rootfs/base/data/demo/animation/32x32_A.png";
 import rootfs_base_data_demo_animation_32x32_B_png from "./rootfs/base/data/demo/animation/32x32_B.png";
 import rootfs_base_data_demo_animation_32x32_C_png from "./rootfs/base/data/demo/animation/32x32_C.png";
@@ -98,6 +103,7 @@ import rootfs_base_data_man_dssh_dsmd from "./rootfs/base/data/man/dssh.dsmd";
 import rootfs_base_data_man_echo_dsmd from "./rootfs/base/data/man/echo.dsmd";
 import rootfs_base_data_man_env_dsmd from "./rootfs/base/data/man/env.dsmd";
 import rootfs_base_data_man_file_dsmd from "./rootfs/base/data/man/file.dsmd";
+import rootfs_base_data_man_fsviewer_dsmd from "./rootfs/base/data/man/fsviewer.dsmd";
 import rootfs_base_data_man_imgview_dsmd from "./rootfs/base/data/man/imgview.dsmd";
 import rootfs_base_data_man_init_dsmd from "./rootfs/base/data/man/init.dsmd";
 import rootfs_base_data_man_less_dsmd from "./rootfs/base/data/man/less.dsmd";
@@ -198,6 +204,9 @@ export function buildrootfs(): DSFileSystem {
      
     binfile = new DSIProcessFile(fs, PRFile);
     curdir.addfile("file", binfile);
+     
+    binfile = new DSIProcessFile(fs, PRFSViewer);
+    curdir.addfile("fsviewer", binfile);
      
     binfile = new DSIProcessFile(fs, PRImgview);
     curdir.addfile("imgview", binfile);
@@ -482,6 +491,30 @@ export function buildrootfs(): DSFileSystem {
     curdir = dirstack.pop();
     // Exited data/app/pixel_assault
         
+    // Traversing data/app/fsviewer
+    dirstack.push(curdir);
+    curdir = curdir.mkdir('fsviewer');
+    
+    // Creating data/app/fsviewer/foldericon.png
+    curfile = new DSIWebFile(fs, rootfs_site_data_app_fsviewer_foldericon_png);
+    curdir.addfile("foldericon.png", curfile);
+    
+    // Creating data/app/fsviewer/fsvieweropen.dsmd
+    curfile = new DSIWebFile(fs, rootfs_site_data_app_fsviewer_fsvieweropen_dsmd);
+    curdir.addfile("fsvieweropen.dsmd", curfile);
+    
+    // Creating data/app/fsviewer/imageicon.png
+    curfile = new DSIWebFile(fs, rootfs_site_data_app_fsviewer_imageicon_png);
+    curdir.addfile("imageicon.png", curfile);
+    
+    // Creating data/app/fsviewer/texticon.png
+    curfile = new DSIWebFile(fs, rootfs_site_data_app_fsviewer_texticon_png);
+    curdir.addfile("texticon.png", curfile);
+    
+    curdir.chmod(DSFilePerms.rx());
+    curdir = dirstack.pop();
+    // Exited data/app/fsviewer
+        
     curdir.chmod(DSFilePerms.rx());
     curdir = dirstack.pop();
     // Exited data/app
@@ -637,6 +670,10 @@ export function buildrootfs(): DSFileSystem {
     // Creating data/man/file.dsmd
     curfile = new DSIWebFile(fs, rootfs_base_data_man_file_dsmd);
     curdir.addfile("file.dsmd", curfile);
+    
+    // Creating data/man/fsviewer.dsmd
+    curfile = new DSIWebFile(fs, rootfs_base_data_man_fsviewer_dsmd);
+    curdir.addfile("fsviewer.dsmd", curfile);
     
     // Creating data/man/imgview.dsmd
     curfile = new DSIWebFile(fs, rootfs_base_data_man_imgview_dsmd);
