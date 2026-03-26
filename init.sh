cfaeaf634df2fc9e78723dce4b0b6839510a431e
Michael Gentili <gentili@mcpnet.ca>
2026-03-26 23:40:32 +0000

    Remove AWS infra init.

--- init.sh
+++ init.sh
@@ -6,12 +6,4 @@ yarn link
 cd $WORKSPACE_DIR
 yarn link @xterm/addon-webgl
 
-echo "Installing AWS CLI"
-curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'
-unzip -o awscliv2.zip
-sudo ./aws/install
-rm -rf awscliv2.zip
-rm -rf ./aws
-
-echo "Hooking up AWS Credentials"
-ln -s $WORKSPACE_DIR/.aws /home/node/.aws
+git submodule update --init --recursive
